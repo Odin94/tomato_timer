88bb63201b655c28478db7499def87ee86ecdb59
Odin <andreas.kammerloher@arcor.de>
2018-05-28 21:00:20 +0200

    Add ids to storage items & a function to retrieve tomatoes

--- lib/tomato_tracker/storage_handler.ex
+++ lib/tomato_tracker/storage_handler.ex
@@ -1,19 +1,19 @@
 # TODO: maybe add projects to group tasks
+
+# Storage is a reversed list; list.first() is the element with the highest ID / latest element
 defmodule StorageHandler do
   def put_task(name) do
     case get_tasks() do
       nil ->
-        PersistentStorage.put(:data, :tasks, [name])
+        PersistentStorage.put(:data, :tasks, [%{id: 1, name: name}])
 
       tasks ->
-        new_tasks =
-          [name | tasks]
-          |> Enum.uniq()
-
+        # TODO: consider not allowing duplicate names
+        new_tasks = [%{id: List.first(tasks).id + 1, name: name} | tasks]
         PersistentStorage.put(:data, :tasks, new_tasks)
     end
 
-    IO.inspect(get_tasks)
+    IO.inspect(get_tasks())
   end
 
   def get_tasks(name \\ nil) do
@@ -22,8 +22,13 @@ defmodule StorageHandler do
         PersistentStorage.get(:data, :tasks)
 
       name ->
-        PersistentStorage.get(:data, :tasks)
-        |> Enum.find(fn task_name -> name == task_name end)
+        tasks = PersistentStorage.get(:data, :tasks)
+
+        if tasks != nil do
+          Enum.find(tasks, fn task_name -> name == task_name end)
+        else
+          nil
+        end
     end
   end
 
@@ -33,5 +38,19 @@ defmodule StorageHandler do
     end
 
     PersistentStorage.put(:data, :tomatoes, %{timestamp: timestamp, task: task})
+  end
+
+  def get_tomatoes(task \\ nil, timestamp \\ nil, id \\ nil) do
+    PersistentStorage.get(:data, :tomatoes)
+    |> Enum.filter(fn t ->
+      cond do
+        (task != nil and t.task != task) or (timestamp != nil and t.timestamp != timestamp) or
+            (id != nil and t.id != id) ->
+          false
+
+        true ->
+          true
+      end
+    end)
   end
 end
