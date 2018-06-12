ba843da7a2682be189266ddc95395d431b6c11b7
Odin <andreas.kammerloher@arcor.de>
2018-06-12 22:50:38 +0200

    Add cookies

--- config/config.exs
+++ config/config.exs
@@ -10,8 +10,7 @@ config :tomato_tracker, TomatoTrackerWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "AdfXy7So1mprVeQLIjLWvKILLaPKGGgG1YAEa2oMNyveSD5lYPfJTQGvQDh28mjU",
   render_errors: [view: TomatoTrackerWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: TomatoTracker.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: TomatoTracker.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -19,10 +18,12 @@ config :logger, :console,
   metadata: [:user_id]
 
 # Configures persistent_storage's tables (each table is one file)
-config :persistent_storage, tables: [
+config :persistent_storage,
+  tables: [
+    config: [path: "./storage/config"],
     data: [path: "./storage/data"]
-]
+  ]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
